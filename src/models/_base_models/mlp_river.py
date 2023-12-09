1a5126e4300dfca9b95ca7fde083138d5c293d2d
shangopi <gopinath.19@cse.mrt.ac.lk>
2023-12-10 05:05:35 +0530

    fixing minor mistake in river-mlp-regression

--- src/models/_base_models/mlp_river.py
+++ src/models/_base_models/mlp_river.py
@@ -1,10 +1,11 @@
-from src.models._base_models.sklearn_base_model import SKLearnBaseModel
 from river import preprocessing as pp
 from river import neural_net as nn
 from river import optim
 
+from src.models._base_models.river_learn_many_base_model import RiverBaseModelMany
 
-class MLPRiverRegressor(SKLearnBaseModel):
+
+class MLPRiverRegressor(RiverBaseModelMany):
     def __init__(self) -> None:
         super().__init__()
         self._params = {}
