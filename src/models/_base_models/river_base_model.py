13fe1bb2fb1dd6b5a5ada8d3b0e144e2f39d92e0
Kajanan Selvanesan <kajananselvanesan@gmail.com>
2024-03-09 15:00:25 +0530

    Refactor

--- src/models/_base_models/river_base_model.py
+++ src/models/_base_models/river_base_model.py
@@ -1,7 +1,4 @@
 from pandas import DataFrame, Series
-from river import stream
-import pandas as pd
-
 from src.models._base_models.ibase_model import IBaseModel
 
 
@@ -26,33 +23,3 @@ class RiverBatchBaseModel(IBaseModel):
 
         ser_prediction: Series = self._model.predict_many(x)
         return ser_prediction.to_frame(name="prediction")
-
-
-class RiverStreamBaseModel(IBaseModel):
-    def __init__(self) -> None:
-        self._model = None
-        self._params = {}
-
-    def fit(self, x, y):
-        if self._model is not None:
-            for xi, yi in stream.iter_pandas(X=x, y=y):
-                self._model = self._model.learn_one(xi, yi)
-            self._model.learn_many(x, y)
-        else:
-            raise NotImplementedError("Model is Empty. Choose appropriate subclasses")
-
-    def incremental_fit(self, ni_x, ni_y):
-        if self._model is not None:
-            for xi, yi in stream.iter_pandas(X=ni_x, y=ni_y):
-                self._model = self._model.learn_one(xi, yi)
-
-    def predict(self, x) -> DataFrame:
-        if len(x) == 0:
-            return DataFrame(columns=["prediction"])
-
-        results = pd.DataFrame()
-        for index, row in x.iterrows():
-            y = self._model.predict_one(row)
-            temp_y = pd.DataFrame({'prediction': [y]})
-            results = pd.concat([results, temp_y], axis=0)
-        return results
